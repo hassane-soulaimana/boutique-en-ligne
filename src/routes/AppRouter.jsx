c9e790e83f3a599557db17c7148042eacfa98050
Ba2 <babasvn19@hotmail.com>
2025-12-02 14:18:57 +0100

    site maj

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,5 +1,6 @@
 // On importe React Router pour gérer la navigation
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
 // On importe le CartProvider
 import { CartProvider } from '../context/CartContext';
@@ -24,11 +25,23 @@ import Panier from '../pages/Panier';
 import Admin from '../pages/Admin';
 import NotFound from '../pages/NotFound';
 
+// Composant qui scroll vers le haut à chaque changement de page
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
+
 // Composant qui gère toutes les routes de notre application
 function AppRouter() {
   return (
     <CartProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           {/* Route parente avec le Layout */}
           <Route path="/" element={<App />}>
